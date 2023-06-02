cc34f229a86e575c8d42a12495b88489a834b156
2ys2 <2ys2bau@gmail.com>
2023-06-02 19:51:53 +0900

    miniproject & labs

--- day47_jquery/WebContent/js/main7.js
+++ day47_jquery/WebContent/js/main7.js
@@ -1,5 +1,49 @@
 /* main7 */
 
+const person = {
+	firstName: 'DoYeon'
+	,lastName: 'Lee'
+	,getFullName: function() {
+		return `${this.firstName} ${this.lastName}`
+	}
+	,setFirstName: function(data){
+		this.firstName = data;
+	}
+}
+console.log(person);
+console.log(person.lastName);
+person.setFirstName('Yuna');
+console.log(person.getFullName());
+console.log(person);
+
+const any = {
+	firstName: 'Any'
+	,lastName: 'Clarke'
+	,getFullName: function() {
+		return `${this.firstName} ${this.lastName}`
+	}
+	,setFirstName: function(data){
+		this.firstName = data;
+	}
+}
+console.log(any);
+console.log(any.firstName);
+any.setFirstName('KimKim')
+console.log(any.getFullName());
+
+
+function User(first, last){
+	this.firstName = first;
+	this.lasttName = last;
+	this.getFullName = function() {
+		return `${this.firstName} ${this.lastName}`
+	}
+}
+const saram = User('person', 'any');
+console.log(saram);
+console.log();
+/*
+
 //this
 //일반(Normal) 함수는 호출 위치에서 this 정의
 //화살표(Arrow) 함수는 자신이 선언한 함수 범위에서 this 정의
@@ -7,6 +51,7 @@
 function User(name) {
 	this.name = name;
 }
+
 /*
 User.prototype.arrow = () => {
 	console.log(this.name);
@@ -18,7 +63,7 @@ const doyeon = {
 		console.log(this.name);
 	}
 }
-*/
+/*
 const doyeon = {
 	name : 'DoYeon',
 	normal : function(){
@@ -33,6 +78,7 @@ const doyeon2 = new User('Yuna')
 doyeon.normal() //DoYeon
 doyeon.arrow()
 
+*/
 
 
 
